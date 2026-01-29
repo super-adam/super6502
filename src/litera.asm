d672e21946c16121f077fd750265ebbc5bff0caf
pawelpawcio <140804416+pawelpawcio@users.noreply.github.com>
2026-01-29 10:43:41 +0100

    Update litera.asm

--- src/litera.asm
+++ src/litera.asm
@@ -1,1 +1,13 @@
+; Definicje adresów
+ACIA_DATA   = $D200
+ACIA_STATUS = $D201
 
+SEND_CHAR:
+    PHA             ; Zachowaj literę na stosie
+WAIT_TX:
+    LDA ACIA_STATUS ; Odczytaj status
+    AND #$10        ; Maskuj bit 4 (Transmit Data Register Empty)
+    BEQ WAIT_TX     ; Jeśli bit = 0 (zajęty), pętla
+    PLA             ; Odzyskaj literę ze stosu
+    STA ACIA_DATA   ; WSTAW LITERĘ TUTAJ - to rozpoczyna wysyłkę
+    RTS
